46a6dbd54193115cbc25cdda9338c93befc4e1d0
Joni <joni@vmi373282.contaboserver.net>
2020-06-15 12:24:36 +0200

    Refactor done

--- index.js
+++ index.js
@@ -6,17 +6,17 @@ var thenrequest = require('then-request');
 var path = require('path');
 var async = require('async');
 
-// app initialization params
 app.set('views', './views');
 app.set('view engine', 'jade');
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Your Api key-secret pair for Authentication
-var zoom_key = 'AcpLIt6NQAq5P1ahVCowLg';
-var zoom_sec = '7WqUH30fsfTk3wC2rSR36prIYMWt465O';
 const request = require('request')
 var router = express.Router();
 
+
+//Your Api key-secret pair for Authentication
+var zoom_key = 'AcpLIt6NQAq5P1ahVCowLg';
+var zoom_sec = '7WqUH30fsfTk3wC2rSR36prIYMWt465O';
 //ACCESS_CODE
 var code;
 //USER_ID
@@ -104,123 +104,6 @@ router.get('/', function(req, res) {
     res.redirect('https://zoom.us/oauth/authorize?response_type=code&client_id=' + process.env.clientID + '&redirect_uri=' + process.env.redirectURL)
 });
 
-
-router.get('/createUser', function(req, res) {
-  res.render('users', {title: 'User Management'});
-});
-
-router.post('/createUser', function(req, res) {
-  
-  var options = {
-    qs: {api_key: zoom_key, api_secret: zoom_sec, data_type: "JSON", email: req.body.email , type: 2}
-  };
-
-  // make an asynchronous request to zoom to create a User
-  var asyncres = thenrequest('POST',"https://dev.zoom.us/v2/user/create",options).done(function (res) {
-    console.log(res.getBody('utf8'));
-    });
-  res.redirect('/');
-});
-
-router.get('/autoUser', function(req, res) {
-  res.render('autoUsers', {title: 'User Management'});
-});
-
-router.post('/autoUser', function(req, res) {
-  console.log(req.body);
-  console.log("email:", req.body.email);
-  var options = {
-    qs: {api_key: zoom_key, api_secret: zoom_sec, data_type: "JSON", email: req.body.email , password: req.body.pwd, type: 2}
-  };
-
-  // make an asynchronous request to zoom to create a user without email verification
-  var asyncres = thenrequest('POST',"https://dev.zoom.us/v2/user/autocreate2",options).done(function (res) {
-    console.log(res.getBody('utf8'));
-    });
-  res.redirect('/');
-});
-
-router.get('/updateUser', function(req, res) {
-  res.render('upUsers', {title: 'User Management'});
-});
-
-router.post('/updateUser', function(req, res) {
-  console.log(req.body);
-  console.log("email:", req.body.id);
-  
-  var options = {
-    qs: {api_key: zoom_key, api_secret: zoom_sec, data_type: "JSON", id: req.body.id , type: req.body.type}
-  };
-
-  // make an asynchronous request to zoom to update a user
-  var asyncres = thenrequest('POST',"https://dev.zoom.us/v2/user/update",options).done(function (res) {
-    console.log(res.getBody('utf8'));
-    });
-  res.redirect('/');
-});
-
-router.get('/createMeeting', function(req, res) {
-  res.render('Meetings', {title: 'Manage Meetings'});
-});
-
-router.post('/createMeeting', function(req, res) {
-  console.log(req.body);
-  console.log("id:", req.body.id);
-
-  console.log("topic:", req.body.topic);
-   var Moptions = {
-    qs: {api_key: zoom_key, api_secret: zoom_sec, data_type: "JSON", host_id: req.body.id , topic: req.body.topic, type: 3}
-  };
-
-  // make an asynchronous request to zoom to create a meeting
-  var asyncres = thenrequest('POST',"https://dev.zoom.us/v2/meeting/create",Moptions).done(function (res) {
-    console.log(res.getBody('utf8'));
-    });
-  res.redirect('/');
-});
-
-router.get('/listMeeting', function(req, res) {
-  res.render('listMeetings', {title: 'Manage Meetings'});
-});
-
-router.post('/listMeeting', function(req, res) {
-  console.log(req.body);
-  console.log("id:", req.body.id);
-
-  var Moptions = {
-    qs: {api_key: zoom_key, api_secret: zoom_sec, data_type: "JSON", host_id: req.body.id }
-  };
-  // make an asynchronous request to zoom to list all meetings
-  var asyncres = thenrequest('POST',"https://dev.zoom.us/v2/meeting/list",Moptions).done(function (res) {
-    console.log(res.getBody('utf8'));
-    });
-  res.redirect('/');
-});
-
-router.get('/updateMeeting', function(req, res) {
-  res.render('upMeetings', {title: 'Manage Meetings'});
-});
-
-router.post('/updateMeeting', function(req, res) {
-  console.log(req.body);
-  console.log("id:", req.body.id);
-
-  console.log("topic:", req.body.topic);
-  var Moptions = {
-    qs: {api_key: zoom_key, api_secret: zoom_sec, data_type: "JSON", host_id: req.body.id , id: req.body.mId, type: req.body.type}
-  };
-  // make an asynchronous request to zoom to update a meeting
-  var asyncres = thenrequest('POST',"https://dev.zoom.us/v2/meeting/update",Moptions).done(function (res) {
-    console.log(res.getBody('utf8'));
-    });
-  res.redirect('/');
-});
-
-router.get('/login', function(req, res) {
-  res.render('meetings', {title: 'Manage Meetings'});
-
-});
-
 router.get('/listmeetings', function(req, res) {
 	var body;
                         console.log("/listmeetings");
@@ -273,13 +156,6 @@ router.get('/myinfo', function(req, res) {
 
 
             if (code) {
-
-                // Step 4:
-                // We can now use the access token to authenticate API calls
-
-                // Send a request to get your user information using the /me context
-                // The `/me` context restricts an API call to the user the token belongs to
-                // This helps make calls to user-specific endpoints instead of storing the userID
 
                 request.get('https://api.zoom.us/v2/users/me',
 				{
@@ -353,7 +229,7 @@ router.get('/home', function(req, res) {
 
 
 
-router.get('/createMeetingV2', function(req, res) {
+router.get('/createMeeting', function(req, res) {
 	
 	
 	var body={
@@ -364,8 +240,6 @@ router.get('/createMeetingV2', function(req, res) {
 								}
 					};
 					
-    console.log("/createMeetingV2");
-
             if (code) {
 
                 request.post('https://api.zoom.us/v2/users/'+user_id+'/meetings',
@@ -409,15 +283,12 @@ router.get('/createMeetingV2', function(req, res) {
 						res.redirect('/');
 
             }
-
-
-//res.redirect('/home');
         return;
 
 });
 
 
-router.get('/createUserV2', function(req, res) {
+router.get('/createUser', function(req, res) {
 	
 	
 	var body={
@@ -429,9 +300,7 @@ router.get('/createUserV2', function(req, res) {
 			"last_name": "Test Surname"
   }
 };
-					
-    console.log("/createUserV2");
-
+				
             if (code) {
 
                 request.post('https://api.zoom.us/v2/users',
@@ -475,29 +344,16 @@ router.get('/createUserV2', function(req, res) {
 						res.redirect('/');
 
             }
-
-
-//res.redirect('/home');
         return;
 
 });
 
 
-
-
-//Eliminando .auth() e incluyendo en el request (TEST)
 router.get('/myrecordings', function(req, res) {
 		var body;
 
 
             if (code) {
-
-                // Step 4:
-                // We can now use the access token to authenticate API calls
-
-                // Send a request to get your user information using the /me context
-                // The `/me` context restricts an API call to the user the token belongs to
-                // This helps make calls to user-specific endpoints instead of storing the userID
 
                 request.get('https://api.zoom.us/v2/users/'+user_id+'/recordings',
 				{
@@ -554,4 +410,4 @@ app.use(bodyParser.json());
 app.use('/', router);
 app.listen(3500);
 
-console.log("Node has started");
+console.log("Zoom App has started.");
