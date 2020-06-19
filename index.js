02fea827f13f92deabd7b4cd951fcfbdac167459
Joni <joni@vmi373282.contaboserver.net>
2020-06-19 12:28:05 +0200

    Create group/List group done

--- index.js
+++ index.js
@@ -405,6 +405,133 @@ router.get('/myrecordings', function(req, res) {
 });
 
 
+
+router.get('/grupos', function(req, res) {
+	var body;
+    request.get('https://api.zoom.us/v2/users/me', (error, response, body) => {
+                    if (error) {
+                        console.log('API Response Error: ', error)
+                    } else {
+                        body = JSON.parse(body);
+                        console.log(body);
+
+                    }
+                }).auth(null, null, true, code);
+  res.render('groups', {title: 'Groups'});
+
+});
+
+
+router.get('/createGroup', function(req, res) {
+	
+	
+	var body={
+					"name": "TestGroup"
+					};
+					
+            if (code) {
+
+                request.post('https://api.zoom.us/v2/groups',
+				{
+				headers: {
+						"content-type": "application/json", 
+				},
+				body: JSON.stringify(body)
+				}, (error, response, body) => {
+
+                    if (error) {
+						console.log(body);
+                        console.log('API Response Error: ', error);
+                    } else {
+                        body = JSON.parse(body);
+                        var JSONResponse = '<pre><code>' + JSON.stringify(body, null, 2) + '</code></pre>'
+						 res.send(`
+                            <style>
+                                @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap');@import url('https://necolas.github.io/normalize.css/8.0.1/normalize.css');html {color: #232333;font-family: 'Open Sans', Helvetica, Arial, sans-serif;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;}h2 {font-weight: 700;font-size: 24px;}h4 {font-weight: 600;font-size: 14px;}.container {margin: 24px auto;padding: 16px;max-width: 720px;}.info {display: flex;align-items: center;}.info>div>span, .info>div>p {font-weight: 400;font-size: 13px;color: #747487;line-height: 16px;}.info>div>span::before {content: "??";}.info>div>h2 {padding: 8px 0 6px;margin: 0;}.info>div>p {padding: 0;margin: 0;}.info>img {background: #747487;height: 96px;width: 96px;border-radius: 31.68px;overflow: hidden;margin: 0 20px 0 0;}.response {margin: 32px 0;display: flex;flex-wrap: wrap;align-items: center;justify-content: space-between;}.response>a {text-decoration: none;color: #2D8CFF;font-size: 14px;}.response>pre {overflow-x: scroll;background: #f6f7f9;padding: 1.2em 1.4em;border-radius: 10.56px;width: 100%;box-sizing: border-box;}
+                            </style>
+                            <div class="container">
+                                <div class="info">
+                                    <div>
+                                        <span>Reuniones</span>
+                                    </div>
+                                </div>
+                                <div class="response">
+                                    <h4>JSON Response:</h4>
+                                    <a href="/home">
+                                        Inicio
+                                    </a>
+                                    ${JSONResponse}
+                                </div>
+                            </div>
+                        `);
+                    }
+                }).auth(null, null, true, code);
+
+            } else {
+                        console.log("Codigo de acceso no existe");
+						res.redirect('/');
+
+            }
+        return;
+
+});
+
+
+router.get('/myGroups', function(req, res) {
+		var body;
+
+
+            if (code) {
+
+                request.get('https://api.zoom.us/v2/groups',
+				{
+				'auth': {
+				'bearer': code
+					}
+					}, (error, response, body) => {
+                    if (error) {
+                        console.log('API Response Error: ', error);
+                    } else {
+                        body = JSON.parse(body);
+                        // Display response in console
+                        //console.log('API call ', body);
+                        // Display response in browser
+                        var JSONResponse = '<pre><code>' + JSON.stringify(body, null, 2) + '</code></pre>'
+                       res.send(`
+                            <style>
+                                @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap');@import url('https://necolas.github.io/normalize.css/8.0.1/normalize.css');html {color: #232333;font-family: 'Open Sans', Helvetica, Arial, sans-serif;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;}h2 {font-weight: 700;font-size: 24px;}h4 {font-weight: 600;font-size: 14px;}.container {margin: 24px auto;padding: 16px;max-width: 720px;}.info {display: flex;align-items: center;}.info>div>span, .info>div>p {font-weight: 400;font-size: 13px;color: #747487;line-height: 16px;}.info>div>span::before {content: "??";}.info>div>h2 {padding: 8px 0 6px;margin: 0;}.info>div>p {padding: 0;margin: 0;}.info>img {background: #747487;height: 96px;width: 96px;border-radius: 31.68px;overflow: hidden;margin: 0 20px 0 0;}.response {margin: 32px 0;display: flex;flex-wrap: wrap;align-items: center;justify-content: space-between;}.response>a {text-decoration: none;color: #2D8CFF;font-size: 14px;}.response>pre {overflow-x: scroll;background: #f6f7f9;padding: 1.2em 1.4em;border-radius: 10.56px;width: 100%;box-sizing: border-box;}
+                            </style>
+                            <div class="container">
+                                <div class="info">
+                                    <div>
+                                        <span>Reuniones</span>
+                                    </div>
+                                </div>
+                                <div class="response">
+                                    <h4>JSON Response:</h4>
+                                    <a href="/home">
+                                        Inicio
+                                    </a>
+                                    ${JSONResponse}
+                                </div>
+                            </div>
+                        `);
+                    }
+                })
+
+            } else {
+                        console.log("Codigo de acceso no existe");
+						res.redirect('/');
+            }
+
+
+
+        return;
+
+    
+
+});
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use('/', router);
